1facc96386347dbd562502cc2b11e252a4ecd8cf
Daniel Kovari <dani.kovari@gmail.com>
2026-09-17 16:53:45 +0200

    Add Dan's stock analysis

---
+++ Dan/app.py
@@ -0,0 +1,11 @@
+import requests
+import json
+import pandas as pd
+import numpy as np
+from pandas import json_normalize
+import yfinance as yf
+import plotly.graph_objects as go
+import plotly.express as px
+
+df = pd.read_csv("Stock_prices_from2012.06")
+change =
